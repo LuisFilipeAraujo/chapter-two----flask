341f974449826b26bc013d4238586deb9f3d28fb
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2022-11-12 01:59:08 -0300

    added comment in routes.py and the requirements

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 
@@ -5,12 +6,12 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - Microblog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>'''
+    return render_template('index.html', title='Home', user=user)
+
+
+''' 
+render_template() function invokes the Jinja2 template engine that 
+comes bundled with the Flask framework. Jinja2 substitutes {{ ... }} 
+blocks with the corresponding values, given by the arguments provided 
+in the render_template() call.
+'''
