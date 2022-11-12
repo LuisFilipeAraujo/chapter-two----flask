15d81333512a14ad69af3e79e8a5643f6a04acda
Luis Filipe Alves Araujo <81663096+LuisFilipeAraujo@users.noreply.github.com>
2022-11-12 01:47:18 -0300

    created mock user "MIGUEL" on routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    from app import app
+
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        user = {'username': 'Miguel'}
+        return '''
+    <html>
+        <head>
+            <title>Home Page - Microblog</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
